29e294060a0edcfc805783902776b2b2f777ce1b
MinHtetTharUTYCC <minhtettharutycc@gmail.com>
2025-12-15 20:51:53 +0700

    updated logo and company name in sidebar

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,52 +1,50 @@
-"use client";
+'use client';
 
-import type { User } from "better-auth";
-import { Van } from "lucide-react";
-import type * as React from "react";
-import { NavMain } from "@/components/sidebar/nav-main";
-import { NavSecondary } from "@/components/sidebar/nav-secondary";
-import { NavUser } from "@/components/sidebar/nav-user";
+import type { User } from 'better-auth';
+import type * as React from 'react';
+import { NavMain } from '@/components/sidebar/nav-main';
+import { NavSecondary } from '@/components/sidebar/nav-secondary';
+import { NavUser } from '@/components/sidebar/nav-user';
 import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
-} from "@/components/ui/sidebar";
-import { data } from "./data";
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+} from '@/components/ui/sidebar';
+import { data } from './data';
+import Link from 'next/link';
+import Image from 'next/image';
+import { paths } from '@/config/paths';
 
 interface AppSidebarProps extends React.ComponentProps<typeof Sidebar> {
-	user: User;
+    user: User;
 }
 
 export function AppSidebar({ user, ...props }: AppSidebarProps) {
-	return (
-		<Sidebar collapsible="offcanvas" {...props}>
-			<SidebarHeader>
-				<SidebarMenu>
-					<SidebarMenuItem>
-						<SidebarMenuButton
-							asChild
-							className="data-[slot=sidebar-menu-button]:p-1.5!"
-						>
-							{/* TODO: Replace with your logo and link */}
-							<div>
-								<Van className="size-5!" />
-								<span className="text-base font-semibold">Acme Inc.</span>
-							</div>
-						</SidebarMenuButton>
-					</SidebarMenuItem>
-				</SidebarMenu>
-			</SidebarHeader>
-			<SidebarContent>
-				<NavMain items={data.navMain} />
-				<NavSecondary items={data.navSecondary} className="mt-auto" />
-			</SidebarContent>
-			<SidebarFooter>
-				<NavUser user={user} />
-			</SidebarFooter>
-		</Sidebar>
-	);
+    return (
+        <Sidebar collapsible="offcanvas" {...props}>
+            <SidebarHeader>
+                <Link
+                    href={paths.dashboard.root.getHref()}
+                    className="flex items-center gap-2 font-semibold text-xl"
+                >
+                    <Image
+                        src="/images/logo.jpg"
+                        width={40}
+                        height={40}
+                        className="rounded-full overflow-hidden"
+                        alt="Logo"
+                    />
+                    7hrs Automobile
+                </Link>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+                <NavSecondary items={data.navSecondary} className="mt-auto" />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={user} />
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
