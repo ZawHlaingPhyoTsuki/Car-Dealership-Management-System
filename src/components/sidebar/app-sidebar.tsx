988150ddb9708aba63fa217b17a5052be0142a7f
MinHtetTharUTYCC <minhtettharutycc@gmail.com>
2025-12-19 22:42:51 +0700

    added copyright at sidebar

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -1,49 +1,61 @@
-"use client";
+'use client';
 
-import type { User } from "better-auth";
-import Image from "next/image";
-import Link from "next/link";
-import type * as React from "react";
-import { NavMain } from "@/components/sidebar/nav-main";
-import { NavUser } from "@/components/sidebar/nav-user";
-import {
-	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarHeader,
-} from "@/components/ui/sidebar";
-import { paths } from "@/config/paths";
-import { data } from "./data";
+import type { User } from 'better-auth';
+import Image from 'next/image';
+import Link from 'next/link';
+import type * as React from 'react';
+import { NavMain } from '@/components/sidebar/nav-main';
+import { NavUser } from '@/components/sidebar/nav-user';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from '@/components/ui/sidebar';
+import { paths } from '@/config/paths';
+import { data } from './data';
 
 interface AppSidebarProps extends React.ComponentProps<typeof Sidebar> {
-	user: User;
+    user: User;
 }
 
 export function AppSidebar({ user, ...props }: AppSidebarProps) {
-	return (
-		<Sidebar collapsible="offcanvas" {...props}>
-			<SidebarHeader>
-				<Link
-					href={paths.dashboard.root.getHref()}
-					className="flex items-center gap-2 font-semibold text-xl"
-				>
-					<Image
-						src="/images/logo.jpg"
-						width={40}
-						height={40}
-						className="rounded-full"
-						alt="7hrs Automobile Logo"
-						priority
-					/>
-					7hrs Automobile
-				</Link>
-			</SidebarHeader>
-			<SidebarContent>
-				<NavMain items={data.navMain} />
-			</SidebarContent>
-			<SidebarFooter>
-				<NavUser user={user} />
-			</SidebarFooter>
-		</Sidebar>
-	);
+    return (
+        <Sidebar collapsible="offcanvas" {...props}>
+            <SidebarHeader>
+                <Link
+                    href={paths.dashboard.root.getHref()}
+                    className="flex items-center gap-2 font-semibold text-xl"
+                >
+                    <Image
+                        src="/images/logo.jpg"
+                        width={40}
+                        height={40}
+                        className="rounded-full"
+                        alt="7hrs Automobile Logo"
+                        priority
+                    />
+                    7hrs Automobile
+                </Link>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+            </SidebarContent>
+            <SidebarFooter>
+                <div className="flex items-center gap-2">
+                    <Image
+                        alt="Banana Coder Avatar"
+                        src="/images/banana-coder.png"
+                        height={40}
+                        width={40}
+                        className="object-cover rounded-full"
+                    />
+                    <div>
+                        <p className="text-xs font-bold">
+                            Banana Coder{' '}
+                            <small className="text-muted-foreground">
+                                &copy; {new Date().getFullYear()}
+                            </small>
+                        </p>
+                    </div>
+                </div>
+                <NavUser user={user} />
+            </SidebarFooter>
+        </Sidebar>
+    );
 }
